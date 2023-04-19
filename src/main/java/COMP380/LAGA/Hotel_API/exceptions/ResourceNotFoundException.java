24a88e55bd53f7db893982ba476f47039a031b0c
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2023-04-19 13:58:58 -0700

    - Accidentally deleted main (lol) - Deleted "Exceptions" package

--- src/main/java/COMP380/LAGA/Hotel_API/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package COMP380.LAGA.Hotel_API.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
