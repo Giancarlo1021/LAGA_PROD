0ea632b2fb66495c5749b5bde5b91f5f7f4d224f
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2023-04-21 17:39:55 -0700

    - Was receiving a stackoverflow error due to HotelRoom object referencing to the Hotel object, which has a reference back to the HotelRoom object, creating an infinite loop. - Fixed thanks to @JsonManagedReference and @JsonBackReference allowing us to specify parent-child relationship between the objects.

--- src/main/java/COMP380/LAGA/Hotel_API/model/HotelRoom.java
+++ src/main/java/COMP380/LAGA/Hotel_API/model/HotelRoom.java
@@ -1,4 +1,87 @@
+//package COMP380.LAGA.Hotel_API.model;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "hotel_rooms")
+//public class HotelRoom {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "hotel_id", nullable = false)
+//    private Hotel hotel;
+//
+//    @Column(name = "room_number", nullable = false)
+//    private Integer roomNumber;
+//
+//    @Column(name = "room_type", nullable = false)
+//    private String roomType;
+//
+//    @Column(nullable = false)
+//    private Double price;
+//
+//    @Column(name = "is_available", nullable = false)
+//    private Boolean isAvailable;
+//
+//    // Getters, setters, and constructors
+//
+//    public HotelRoom() {
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Hotel getHotel() {
+//        return hotel;
+//    }
+//
+//    public void setHotel(Hotel hotel) {
+//        this.hotel = hotel;
+//    }
+//
+//    public Integer getRoomNumber() {
+//        return roomNumber;
+//    }
+//
+//    public void setRoomNumber(Integer roomNumber) {
+//        this.roomNumber = roomNumber;
+//    }
+//
+//    public String getRoomType() {
+//        return roomType;
+//    }
+//
+//    public void setRoomType(String roomType) {
+//        this.roomType = roomType;
+//    }
+//
+//    public Double getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(Double price) {
+//        this.price = price;
+//    }
+//
+//    public Boolean getIsAvailable() {
+//        return isAvailable;
+//    }
+//
+//    public void setIsAvailable(Boolean isAvailable) {
+//        this.isAvailable = isAvailable;
+//    }
+//}
 package COMP380.LAGA.Hotel_API.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
 
@@ -10,8 +93,9 @@ public class HotelRoom {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hotel_id", nullable = false)
+    @JsonBackReference
     private Hotel hotel;
 
     @Column(name = "room_number", nullable = false)
@@ -79,3 +163,6 @@ public class HotelRoom {
         this.isAvailable = isAvailable;
     }
 }
+
+
+
