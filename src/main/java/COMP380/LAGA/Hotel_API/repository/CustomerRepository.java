eebb94f0f55b26aac555b05204cbe51ab8f2766a
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2023-04-26 17:25:52 -0700

    Pushing empty repository to fix missing folders and packages

--- src/main/java/COMP380/LAGA/Hotel_API/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package COMP380.LAGA.Hotel_API.repository;
-
-import COMP380.LAGA.Hotel_API.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
