9ddf9c8b24d64afecb5d4a488c3c6b7773f738a5
AH-Neuro <125938682+AH-Neuro@users.noreply.github.com>
2023-05-01 15:22:41 -0700

    Add files via upload
    Updated controllers to include employee authentication. I was unfortunately unable to test the methods due to issues with my Eclipse installation, and am exploring a solution for this.

---
+++ CustomerController.java
@@ -0,0 +1,70 @@
+package COMP380.LAGA.Hotel_API.controller;
+
+import COMP380.LAGA.Hotel_API.model.Customer;
+import COMP380.LAGA.Hotel_API.repository.CustomerRepository;
+import COMP380.LAGA.Hotel_API.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @CrossOrigin
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        if (customer == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(customer);
+    }
+
+    @CrossOrigin
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Integer id, @RequestBody Customer updatedCustomer) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        if (customer == null) {
+            return ResponseEntity.notFound().build();
+        }
+        customer.setFirstName(updatedCustomer.getFirstName()); // Corrected this line
+        customer.setLastName(updatedCustomer.getLastName());
+        customer.setEmail(updatedCustomer.getEmail());
+        customer.setPhoneNumber(updatedCustomer.getPhoneNumber());
+        Customer savedCustomer = customerRepository.save(customer);
+        return ResponseEntity.ok(savedCustomer);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Integer id, @PathVariable Integer employeeId) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (customer != null && employee != null) {
+            customerRepository.delete(customer);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ TransactionController.java
@@ -0,0 +1,169 @@
+package COMP380.LAGA.Hotel_API.controller;
+
+import COMP380.LAGA.Hotel_API.dto.CreateTransactionRequestDto;
+import COMP380.LAGA.Hotel_API.dto.TransactionCustomerRoomDto;
+import COMP380.LAGA.Hotel_API.model.*;
+import COMP380.LAGA.Hotel_API.repository.*;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private HotelRoomRepository hotelRoomRepository;
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<List<TransactionCustomerRoomDto>> getAllTransactions() {
+        List<TransactionCustomerRoomDto> transactions = transactionRepository.findAllTransactionCustomerRoomInfo();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @GetMapping("/customer-room-info")
+    public List<TransactionCustomerRoomDto> getAllTransactionCustomerRoomInfo() {
+        return transactionRepository.findAllTransactionCustomerRoomInfo();
+    }
+
+    @CrossOrigin
+    @GetMapping("/get-by-email/{customerEmail}")
+    public ResponseEntity<List<TransactionCustomerRoomDto>> getTransactionsByEmail(@PathVariable("customerEmail") String customerEmail) {
+        List<TransactionCustomerRoomDto> transactions = transactionRepository.findByCustomerEmail(customerEmail);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping("/get-by-transaction/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Integer id) {
+        Transaction transaction = transactionRepository.findByIdWithCustomer(id);
+        if (transaction == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(transaction);
+    }
+
+//    @CrossOrigin
+//    @PostMapping("/hotels/{hotelId}/transactions")
+//    public ResponseEntity<Transaction> createTransaction(@PathVariable Long hotelId, @RequestBody CreateTransactionRequestDto request) {
+//        // Check if hotel exists
+//        Optional<Hotel> hotelOptional = hotelRepository.findById(hotelId);
+//        if (!hotelOptional.isPresent()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        Hotel hotel = hotelOptional.get();
+//
+//        // Check if hotel room exists
+//        Optional<HotelRoom> hotelRoomOptional = hotelRoomRepository.findById(request.getHotelRoomId());
+//        if (!hotelRoomOptional.isPresent() || !hotelRoomOptional.get().getHotel().equals(hotel)) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//        HotelRoom hotelRoom = hotelRoomOptional.get();
+//
+//        // Check if customer already exists or create new customer
+//        Customer customer;
+//        Optional<Customer> customerOptional = customerRepository.findByEmail(request.getCustomer().getEmail());
+//        if (customerOptional.isPresent()) {
+//            customer = customerOptional.get();
+//        } else {
+//            customer = request.getCustomer();
+//            customerRepository.save(customer);
+//        }
+//
+//        // Create new transaction and save to repository
+//        Transaction transaction = new Transaction();
+//        transaction.setCustomer(customer);
+//        transaction.setHotelRoom(hotelRoom);
+//        transaction.setCheckInDate(request.getCheckInDate());
+//        transaction.setCheckOutDate(request.getCheckOutDate());
+//        transaction.setTotalAmount(request.getTotalAmount());
+//        Transaction savedTransaction = transactionRepository.save(transaction);
+//
+//        return ResponseEntity.ok(savedTransaction);
+//    }
+
+    @CrossOrigin
+    @PostMapping("/hotels/{hotelId}/transactions")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody CreateTransactionRequestDto request) {
+        Customer customer;
+        Optional<Customer> customerOptional = customerRepository.findByEmail(request.getCustomer().getEmail());
+        Optional<HotelRoom> hotelRoomOptional = hotelRoomRepository.findById(request.getHotelRoomId());
+
+        if (customerOptional.isPresent()) {
+            customer = customerOptional.get();
+            System.out.println("Customer found: " + customer);
+        } else {
+            customer = customerRepository.save(request.getCustomer());
+            System.out.println("New customer created: " + customer);
+        }
+
+        if (hotelRoomOptional.isPresent()) {
+            HotelRoom hotelRoom = hotelRoomOptional.get();
+
+            Transaction transaction = new Transaction();
+            transaction.setCustomer(customer);
+            transaction.setHotelRoom(hotelRoom);
+            transaction.setCheckInDate(request.getCheckInDate());
+            transaction.setCheckOutDate(request.getCheckOutDate());
+            transaction.setTotalAmount(request.getTotalAmount());
+
+            Transaction savedTransaction = transactionRepository.save(transaction);
+            System.out.println("New transaction created: " + savedTransaction);
+            return new ResponseEntity<>(savedTransaction, HttpStatus.CREATED);
+        } else {
+            System.out.println("Hotel room not found.");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> updateTransaction(@PathVariable Integer id, @RequestBody Transaction transaction) {
+        Optional<Transaction> existingTransaction = transactionRepository.findById(id);
+        if (existingTransaction.isPresent()) {
+            Transaction updatedTransaction = existingTransaction.get();
+            updatedTransaction.setCustomer(transaction.getCustomer());
+            updatedTransaction.setHotelRoom(transaction.getHotelRoom());
+            updatedTransaction.setCheckInDate(transaction.getCheckInDate());
+            updatedTransaction.setCheckOutDate(transaction.getCheckOutDate());
+            updatedTransaction.setTotalAmount(transaction.getTotalAmount());
+            transactionRepository.save(updatedTransaction);
+            return new ResponseEntity<>(updatedTransaction, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Integer transactionId, @PathVariable Integer employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElse(null);
+        if (employee != null) {
+            Transaction transaction = transactionRepository.findById(transactionId).orElse(null);
+            if (transaction == null) {
+                return ResponseEntity.notFound().build();
+            }
+            transactionRepository.delete(transaction);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
